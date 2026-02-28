821e253444a336e1ce7f5cfa77685338da0a66f4
myNameIsCK <4739494@qq.com>
2026-02-28 15:25:09 +0800

    add Mx2dView source code

---
+++ src/mxcad2d/Mx2dView.cpp
@@ -0,0 +1,15 @@
+﻿/******************************************************************************************
+Copyright (c) 2025 Chengdu Dreamkaide Technology Co., Ltd. <https://www.webcadsdk.com/>
+All rights reserved.
+Applications incorporating this software must include the following copyright notice.
+This application shall enter into an agreement with Chengdu Dreamkaide Technology Co., Ltd.
+for the use of this software, its documentation or related materials.
+*******************************************************************************************/
+
+#include "Mx2dView.h"
+
+Mx2dView::Mx2dView(QWidget* parent)
+    : QWidget(parent)
+{
+}
+
---
+++ src/mxcad2d/Mx2dView.h
@@ -0,0 +1,22 @@
+﻿/******************************************************************************************
+Copyright (c) 2025 Chengdu Dreamkaide Technology Co., Ltd. <https://www.webcadsdk.com/>
+All rights reserved.
+Applications incorporating this software must include the following copyright notice.
+This application shall enter into an agreement with Chengdu Dreamkaide Technology Co., Ltd.
+for the use of this software, its documentation or related materials.
+*******************************************************************************************/
+
+#pragma once
+
+
+#include <QWidget>
+
+class Mx2dView : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Mx2dView(QWidget *parent = nullptr);
+signals:
+
+public slots:
+};
